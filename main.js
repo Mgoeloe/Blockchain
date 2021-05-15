f8d5c4952d64f849d9e19a50d7ada5137219bdb0
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-05-15 20:01:25 +0200

    Inplemented Proof-of-Work

--- main.js
+++ main.js
@@ -7,16 +7,28 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
+
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -29,7 +41,7 @@ class Blockchain{
 
     addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
     }
 
@@ -52,13 +64,14 @@ class Blockchain{
 }
 
 let marlonCoin = new Blockchain();
+
+console.log('Mining block 1...')
 marlonCoin.addBlock(new Block(1, "02/01/2021", {amount: 4}));
+
+console.log('Mining block 2...')
 marlonCoin.addBlock(new Block(2, "08/01/2021", {amount: 10}));
 
-console.log(JSON.stringify(marlonCoin, null, 4));
+console.log('Mining block 3...')
+marlonCoin.addBlock(new Block(3, "10/01/2021", {amount: 5}));
 
-// marlonCoin.chain[1].data = {amount: 99}
-// marlonCoin.chain[1].hash = marlonCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + marlonCoin.isChainValid());
 
